bdf1f9ef8bf6f564f578cd941910fec14fb2d96f
franias <franciellipdias@gmail.com>
2019-10-16 21:05:59 -0300

    start the step

--- features/step-definitions/test_sicredi.js
+++ features/step-definitions/test_sicredi.js
@@ -1,3 +1,6 @@
-module.exports = function () {
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+const {Builder, By, until} = require('selenium-webdriver');
+const {simulationPageObjects} = require('../../page-objects/simulation-page.js');
 
-};
+let driver = new Builder().forBrowser('chrome').build();
