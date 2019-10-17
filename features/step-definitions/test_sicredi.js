f8ebf3e20b716ef02ec2dd1a2cdf8fffe39daa0b
franias <franciellipdias@gmail.com>
2019-10-17 02:41:51 -0300

    alter configurations

--- features/step-definitions/test_sicredi.js
+++ features/step-definitions/test_sicredi.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 const {Builder, By, until} = require('selenium-webdriver');
-const {simulationPageObjects} = require('../../page-objects/simulation-page.js');
+// const {simulationPageObjects} = require('../../page-objects/simulation-page.js');
 
 let driver = new Builder().forBrowser('chrome').build();
